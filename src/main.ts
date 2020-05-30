bc22e8ce46ad6f63f983542427b4851f133f4082
super-programmer <zkp593655843@163.com>
2020-05-30 21:38:32 +0800

    feat:docker部署

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import 'vant/lib/index.css';
 import '@/assets/styles/common.css'
 import {
   Button,
